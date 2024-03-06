a082fe4d9475a90f50f10d3ba27651f60e663a15
Karankumar2004 <kk8594879@gmail.com>
2024-03-07 03:01:35 +0530

    Add Ass3_Q5

--- Assignment_3/Ass3_Q4.cpp
+++ Assignment_3/Ass3_Q4.cpp
@@ -6,53 +6,61 @@ class MyStack
 private:
     int max_size;
     int *arr;
-    int top; //Accessing this in the main that's it is in the public
-    static int count; //Static why?? //ek hi copy har jagah share hoga
+    int top;          // Accessing this in the main that's it is in the public
+    static int count; // Static why?? //ek hi copy har jagah share hoga
     static const int Max_Stack = 5;
+
 public:
-    
-    //Making Constructor
-    MyStack(int size){
-        if(count>= Max_Stack){
-            cout<<"Maximum limit reached"<<endl;
+    // Making Constructor
+    MyStack(int size)
+    {
+        if (count >= Max_Stack)
+        {
+            cout << "Maximum limit reached" << endl;
             max_size = 0;
             arr = nullptr;
             top = -1;
         }
 
+        // otherwise
         max_size = size;
         arr = new int[size];
-        top=-1;
+        top = -1;
         count++;
     }
 
-    //Copy Constructor
-    MyStack(MyStack &A){
+    // Copy Constructor
+    MyStack(MyStack &A)
+    {
         max_size = A.max_size;
-        arr = new int[A.max_size]; //Dynamically allocating the same size of memory as A
-        for(int i=0;i<max_size;i++){
-            arr[i] = A.arr[i];      //Copying the value
+        arr = new int[A.max_size]; // Dynamically allocating the same size of memory as A
+        for (int i = 0; i < max_size; i++)
+        {
+            arr[i] = A.arr[i]; // Copying the value
         }
         top = A.top;
         count++;
     }
 
-    //Operator overloading
-    MyStack operator+(const MyStack &st){
-        MyStack st_3(max_size + st.max_size); //Making the size of resultant stack is the sum two stack
-        
-        //Pushing the elements of first stack into result stack
-        for(int i=0;i<=top;i++){
+    // Operator overloading
+    MyStack operator+(const MyStack &st)
+    {
+        MyStack st_3(max_size + st.max_size); // Making the size of resultant stack is the sum two stack
+
+        // Pushing the elements of first stack into result stack
+        for (int i = 0; i <= top; i++)
+        {
             st_3.Push(arr[i]);
         }
 
-        //Pushing the elements of second stack into result stack
-        for(int i=0; i<=st.top;i++){
+        // Pushing the elements of second stack into result stack
+        for (int i = 0; i <= st.top; i++)
+        {
             st_3.Push(st.arr[i]);
         }
         return st_3;
     }
-    
+
     int Push(int a)
     {
         if (top == max_size)
@@ -71,11 +79,12 @@ public:
         // How to delete the memory
         top--;
     }
-    
-    int Top() { //For accessing the top element
-    return arr[top];
-  }
-  
+
+    int Top()
+    { // For accessing the top element
+        return arr[top];
+    }
+
     int CurrentSize()
     {
         return top + 1; // As top starts from 0 so the size is top+1
@@ -93,70 +102,49 @@ public:
         }
     }
 
-    static int getCount() {
+    void show() // For printing the values
+    {
+        cout << "Stack Content: ";
+        for (int i = 0; i <= top; ++i)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    static int getCount()
+    {
         return count;
     }
 };
 
-int MyStack :: count=0; //We can acsess it without creating any object
+int MyStack ::count = 0; // We can acsess it without creating any object
 
-int main(){
-    
-    int n1;
-    cout<<"ENter the size of Stack"<<endl;
-    cin>>n1;
+int main()
+{
 
-    MyStack s1(n1); //Creating object of MyStack class of size n
-    
-    cout<<"Enter the stack value\n";
-    for(int i=0;i<n1;i++){ //Pushing the value into the stack
-        int temp;
-        cin>>temp;
-        s1.Push(temp);
-    }
-    
-    cout<<"\nPrinting the top element \n";
-    cout<<s1.Top();
+    MyStack stack1(5);
+    stack1.Push(11);
+    stack1.Push(44);
+    stack1.Push(56);
+    stack1.Push(55);
+    stack1.show();
 
-    cout<<"\nSize of stack: "<<s1.CurrentSize();
+    MyStack stack2(3);
+    stack2.Push(75);
+    stack2.Push(25);
+    stack2.Push(85);
+    stack2.show();
 
-    s1.Pop();
-    cout<<"\nPrinting the top element after popping\n";
-    cout<<s1.Top();
+    MyStack stack3 = stack1 + stack2;
+    stack3.show();
 
-    cout<<"\nSize of stack: "<<s1.CurrentSize();
-    s1.IsEmpty();
-    cout<<"\nNo. of Stack: "<<MyStack::getCount()<<endl;
-
-   /**********Copy Constructor************/
-   cout<<"\nStack-2: ";
-   MyStack s2 = s1;
-   for(int i=0;i<4;i++){
-    cout<<s2.Top()<<"  ";
-    s2.Pop();
-   }
-   cout<<"\nNo. of Stack: "<<MyStack::getCount()<<endl;
-
-   /**********Assignement Operator************/
- /*  MyStack s3(4);
-   for(int i=3;i>=0;i--){
-    s3.Push(s1.Top());
-    s1.Pop();
-   } 
-   for(int i=0;i<4;i++){
-    cout<<s3.Top()<<"  ";
-    s3.Pop();
-   }
-    cout<<"\nNo. of Stack: "<<MyStack::getCount()<<endl;
-*/
-    cout<<"\nStack-3: ";
-    MyStack s3 = (s1+s2);
-    for(int i=0;i<8;i++){
-    cout<<s3.Top()<<"  ";
-    s3.Pop();
-   }
-
-
+    // Attempt to create more stacks than the maximum allowed
+    MyStack stack4(2);
+    MyStack stack5(8);
+    MyStack stack6(11);
+    MyStack stack7(87);
+    MyStack stack8(16);
 
     return 0;
 }
---
+++ Assignment_3/Ass4_Q5.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int coefficient; // For the coeffcient of polynomial
+    int exponent;    // For the power of x(polynomial)
+    node *next;
+
+public:
+    // Constructor
+    node(int coeff, int exp)
+    {
+        this->coefficient = coeff;
+        this->exponent = exp;
+        this->next = NULL; // For that node next is NULL
+    }
+};
+
+class List
+{
+private:
+    node *head; // jo ki linked list ke head ko point karegaa
+
+public:
+    // Constructor
+    List()
+    {
+        this->head = NULL;
+    }
+
+    // For pushing the node in the linked list
+    void push(int coeff, int exp)
+    {
+        node *newNode = new node(coeff, exp); // Creating a node which have coeff and exponent as given
+        if (head == NULL)
+        { // case where linked list is empty
+            head = newNode;
+        }
+        else
+        {
+            node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next; // taking the pointer to the last node
+            }
+            temp->next = newNode; // means we are inserting node at end
+        }
+    }
+
+    // For printing the polynomial
+    void show()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->coefficient << "x^" << temp->exponent; // Printing the node value
+            temp = temp->next;                                   // Shifting to the next node
+            if (temp != NULL)
+            {
+                cout << "+";
+            }
+        }
+        cout << endl;
+    }
+
+    // Now adding the polynomial
+    List operator+(List &other)
+    {
+        List result;
+        node *temp1 = head; // That means pointing to this ka head
+        node *temp2 = other.head;
+
+        while (temp1 != NULL && temp2 != NULL)
+        {
+            if (temp1->exponent > temp2->exponent) // Agar power temp1 wale list ka large hoga
+            {
+                result.push(temp1->coefficient, temp1->exponent);
+                temp1 = temp1->next;
+            }
+            else if (temp1->exponent < temp2->exponent) // Agar power temp2 wale list ka large hoga
+            {
+                result.push(temp2->coefficient, temp2->exponent);
+                temp2 = temp2->next;
+            }
+            else
+            { // Agar dono ka exponent equal ho
+                int sum = temp1->coefficient + temp2->coefficient;
+                result.push(sum, temp1->exponent); // We can also push temp2->exponent as both are same
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+            }
+        }
+
+        // Agar abb kuch value bach gye in any list
+
+        while (temp1 != NULL)
+        {
+            result.push(temp1->coefficient, temp1->exponent);
+            temp1 = temp1->next;
+        }
+
+        while (temp2 != NULL)
+        {
+            result.push(temp2->coefficient, temp2->exponent);
+            temp2 = temp2->next;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    List l1, l2;
+    l1.push(2, 3);          // 2x^3
+    l1.push(1, 2);          // x^2
+    l1.push(5, 1);          // x
+    l1.push(5, 0);          // 5;
+    cout << "Equation-1: "; // 2x^3+x^2x+5
+    l1.show();
+
+    l2.push(4, 3);          // 4x^3
+    l2.push(5, 1);          // x
+    l2.push(8, 0);          // 8;
+    cout << "Equation-1: "; // 4x^3+x+8
+    l2.show();
+
+    List l3 = l1 + l2;
+    cout << "Sum of two equation: ";
+    l3.show();
+
+    return 0;
+}
